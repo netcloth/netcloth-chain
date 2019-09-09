3b750676ccef77654f64e39e9388e83fd82bc301
protx <protx123456@protonmail.com>
2019-09-09 10:00:41 +0800

    add mint module

---
+++ x/mint/internal/types/codec.go
@@ -0,0 +1,14 @@
+package types
+
+import (
+	"github.com/NetCloth/netcloth-chain/codec"
+)
+
+// generic sealed codec to be used throughout this module
+var ModuleCdc *codec.Codec
+
+func init() {
+	ModuleCdc = codec.New()
+	codec.RegisterCrypto(ModuleCdc)
+	ModuleCdc.Seal()
+}
