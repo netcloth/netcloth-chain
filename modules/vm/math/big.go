b5468780446ddf6a26cbd48b240ba7bb5f37e5e2
iavl <z6521256@gmail.com>
2019-11-21 19:04:21 +0800

    add vm memory

---
+++ modules/vm/math/big.go
@@ -0,0 +1,23 @@
+package math
+
+import "math/big"
+
+const (
+	// number of bits in a big.Word
+	wordBits = 32 << (uint64(^big.Word(0)) >> 63)
+	// number of bytes in a big.Word
+	wordBytes = wordBits / 8
+)
+
+// ReadBits encodes the absolute value of bigint as big-endian bytes. Callers must ensure
+// that buf has enough space. If buf is too short the result will be incomplete.
+func ReadBits(bigint *big.Int, buf []byte) {
+	i := len(buf)
+	for _, d := range bigint.Bits() {
+		for j := 0; j < wordBytes && i > 0; j++ {
+			i--
+			buf[i] = byte(d)
+			d >>= 8
+		}
+	}
+}
