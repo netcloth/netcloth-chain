795c8348b519638e5dd984e4ecd53c3360b45c10
iavl <z6521256@gmail.com>
2019-12-06 19:05:35 +0800

    add vm handler test

--- modules/vm/handler_test.go
+++ modules/vm/handler_test.go
@@ -4,11 +4,20 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/netcloth/netcloth-chain/modules/vm/types"
+
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdk "github.com/netcloth/netcloth-chain/types"
 )
+
+func newSdkAddress() sdk.AccAddress {
+	tmpKey := secp256k1.GenPrivKey().PubKey()
+	return sdk.AccAddress(tmpKey.Address().Bytes())
+}
 
 func TestInvalidMsg(t *testing.T) {
 	k := Keeper{}
@@ -17,4 +26,19 @@ func TestInvalidMsg(t *testing.T) {
 	res := h(sdk.NewContext(nil, abci.Header{}, false, nil), sdk.NewTestMsg())
 	require.False(t, res.IsOK())
 	require.True(t, strings.Contains(res.Log, "Unrecognized Msg type"))
+}
+
+func TestMsgContractCreate(t *testing.T) {
+	fromAddr := newSdkAddress()
+	amount := sdk.NewInt64Coin(sdk.NativeTokenName, 150)
+	code := []byte("xxxx")
+
+	msg := types.NewMsgContractCreate(fromAddr, amount, code)
+	require.NotNil(t, msg)
+	require.Equal(t, msg.Route(), RouterKey)
+	require.Equal(t, msg.Type(), types.TypeMsgContractCreate)
+
+}
+
+func TestMsgContractCall(t *testing.T) {
 }
