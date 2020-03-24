c918b559e669e406398c26f3391bf7f5d72fbba8
iavl <z6521256@gmail.com>
2020-03-24 20:47:00 +0800

    update vm EndBlock

--- modules/vm/module.go
+++ modules/vm/module.go
@@ -109,11 +109,19 @@ func (a AppModule) BeginBlock(sdk.Context, abci.RequestBeginBlock) {
 }
 
 func (a AppModule) EndBlock(ctx sdk.Context, end abci.RequestEndBlock) []abci.ValidatorUpdate {
+	// Gas costs are handled within msg handler so costs should be ignored
+	ebCtx := ctx.WithBlockGasMeter(sdk.NewInfiniteGasMeter())
+
+	// Update account balances before committing other parts of state
 	a.k.StateDB.UpdateAccounts()
-	_, err := a.k.StateDB.WithContext(ctx).Commit(true)
+
+	// Commit state objects to KV store
+	_, err := a.k.StateDB.WithContext(ebCtx).Commit(true)
 	if err != nil {
 		panic(err)
 	}
+
+	// Clear accounts cache after account data has been committed
 	a.k.StateDB.ClearStateObjects()
 
 	return []abci.ValidatorUpdate{}
