b65df9704d2c65700fe230f98ed142336d6f626b
cpucorecore <cpucorecore@gmail.com>
2020-04-28 16:04:54 +0800

    test: fix testcase: checksum should changed for the main prefix is changed to 'nch'

--- crypto/keys/types_test.go
+++ crypto/keys/types_test.go
@@ -25,7 +25,7 @@ func Test_writeReadLedgerInfo(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "44'/118'/5'/0/1", path.String())
 	assert.Equal(t,
-		"nchpub1addwnpepqddddqg2glc8x4fl7vxjlnr7p5a3czm5kcdp4239sg6yqdc4rc2r5wmxv8p",
+		"nchpub1addwnpepqddddqg2glc8x4fl7vxjlnr7p5a3czm5kcdp4239sg6yqdc4rc2r56cc7zn",
 		types.MustBech32ifyPubKey(sdk.Bech32PubKeyTypeAccPub, lInfo.GetPubKey()))
 
 	// Serialize and restore
