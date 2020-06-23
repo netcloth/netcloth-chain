886dd7d87d67a5b4765dece00d8c9f8c5205b384
iavl <z6521256@gmail.com>
2020-06-23 17:59:25 +0800

    update travis

---
+++ go.test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.txt
+
+go test ./... -mod=readonly -timeout 12m -race -coverprofile=coverage.txt -covermode=atomic
+
+# filter out DONTCOVER
+excludelist="$(find ./ -type f -name '*.go' | xargs grep -l 'DONTCOVER')"
+excludelist+=" $(find ./ -type f -name '*.pb.go')"
+excludelist+=" $(find ./ -type f -path './tests/mocks/*.go')"
+for filename in ${excludelist}
+do
+  filename=$(echo $filename | sed 's/^./github.com\/cosmos\/cosmos-sdk/g')
+  echo "Excluding ${filename} from coverage report..."
+  sed -i.bak "/$(echo $filename | sed 's/\//\\\//g')/d" coverage.txt
+done
