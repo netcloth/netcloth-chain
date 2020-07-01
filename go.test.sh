df1fe1ca9dfea74b4ebfa4c07be03ef96177e06a
iavl <z6521256@gmail.com>
2020-07-01 16:21:49 +0800

    update go.test script

--- go.test.sh
+++ go.test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-go test -mod=readonly -timeout 12m -race -coverprofile=coverage.txt -covermode=atomic \
+go test -mod=readonly -timeout 20m -race -coverprofile=coverage.txt -covermode=atomic \
 $(go list ./... | grep -v 'server' | grep -v '/simulation' | grep -v mock | grep -v 'netcloth-chain/tests' | grep -v 'crypto' | grep -v '/simapp')
 
 # filter out DONTCOVER
