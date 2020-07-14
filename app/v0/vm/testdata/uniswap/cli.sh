bc05daa0d48dbf0ef49e5b12cbdc6277a7a22c01
iavl <z6521256@gmail.com>
2020-07-14 17:49:04 +0800

    update test bytecode

--- app/v0/vm/testdata/uniswap/cli.sh
+++ app/v0/vm/testdata/uniswap/cli.sh
@@ -8,15 +8,24 @@ nchcli vm create --code_file=./uniswap.bc \
 
 echo 11111111 | \
 nchcli vm call --from=$(nchcli keys show -a alice) \
---contract_addr=nch1crgfeph6hl9z9hw0eevym6p26y85wy4gxle3fq \
+--contract_addr=nch1hmljmh2n4esr6de8fd9jjnsth3ty0ux7d7082m \
 --method=initializeFactory \
 --abi_file="./uniswap.abi" \
 --args="nch1w8x5hxmdwjz7pw0kwukmgaavnpu5q3e0churjj" \
 --gas=98669 -b block -y
 
+# out of gas
 echo 11111111 | \
 nchcli vm call --from=$(nchcli keys show -a alice) \
---contract_addr=nch1crgfeph6hl9z9hw0eevym6p26y85wy4gxle3fq \
+--contract_addr=nch1hmljmh2n4esr6de8fd9jjnsth3ty0ux7d7082m \
+--method=createExchange \
+--abi_file=./uniswap.abi \
+--args="nch1w8x5hxmdwjz7pw0kwukmgaavnpu5q3e0churjj" \
+--gas=3727089 -b block -y
+
+echo 11111111 | \
+nchcli vm call --from=$(nchcli keys show -a alice) \
+--contract_addr=nch1hmljmh2n4esr6de8fd9jjnsth3ty0ux7d7082m \
 --method=createExchange \
 --abi_file=./uniswap.abi \
 --args="nch1w8x5hxmdwjz7pw0kwukmgaavnpu5q3e0churjj" \
