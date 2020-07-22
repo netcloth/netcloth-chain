3c9617420b1143060958dcb30a7a4953c8ce8c8f
cpucorecore <cpucorecore@gmail.com>
2020-07-22 15:56:10 +0800

    add comments

--- app/v0/keeper_accessor.go
+++ app/v0/keeper_accessor.go
@@ -1,5 +1,3 @@
-// nolint
-
 package v0
 
 import (
@@ -9,22 +7,27 @@ import (
 	"github.com/netcloth/netcloth-chain/app/v0/supply"
 )
 
+// GovKeeper return govKeeper
 func (p *ProtocolV0) GovKeeper() gov.Keeper {
 	return p.govKeeper
 }
 
+// SetGovKeeper set govKeeper
 func (p *ProtocolV0) SetGovKeeper(gk gov.Keeper) {
 	p.govKeeper = gk
 }
 
+// StakingKeeper return stakingKeeper
 func (p *ProtocolV0) StakingKeeper() staking.Keeper {
 	return p.stakingKeeper
 }
 
+// AccountKeeper return accountKeeper
 func (p *ProtocolV0) AccountKeeper() auth.AccountKeeper {
 	return p.accountKeeper
 }
 
+// SupplyKeeper return supplyKeeper
 func (p *ProtocolV0) SupplyKeeper() supply.Keeper {
 	return p.supplyKeeper
 }
