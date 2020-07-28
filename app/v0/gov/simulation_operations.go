b755c3beb80ecb7f969d6b7be46d176bb10299e0
iavl <z6521256@gmail.com>
2020-07-28 15:44:07 +0800

    add unit test

--- app/v0/gov/simulation_operations.go
+++ app/v0/gov/simulation_operations.go
@@ -1,5 +1,7 @@
 package gov
 
+// DONTCOVER
+
 import (
 	"math"
 	"math/rand"
--- go.test.sh
+++ go.test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 go test -mod=readonly -timeout 20m -race -coverprofile=coverage.txt -covermode=atomic \
-$(go list ./... | grep -v 'server' | grep -v '/simulation' | grep -v mock | grep -v 'netcloth-chain/tests' | grep -v 'crypto' | grep -v '/simapp' | grep -v '/app/genesis')
+$(go list ./... | grep -v 'netcloth-chain/server' | grep -v '/simulation' | grep -v mock | grep -v 'netcloth-chain/tests' | grep -v 'crypto' | grep -v '/simapp' | grep -v '/app/genesis')
 
 # filter out DONTCOVER
 excludelist="$(find . -type f -name '*.go' | xargs grep -l 'DONTCOVER')"
